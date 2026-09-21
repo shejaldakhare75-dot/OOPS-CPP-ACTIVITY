d69c1af12fee4633a098676c1e91a8f931c5f3a4
shejaldakhare75-dot <shejaldakhare75@gmail.com>
2026-09-21 22:25:59 +0530

    Added Program 03

---
+++ Unit-01/CodeBook/Program03.cpp
@@ -0,0 +1,9 @@
+#include &lt;iostream&gt;
+using namespace std;
+int main() {
+int marks[5] = {78, 82, 91, 67, 88};
+for (int i = 0; i &lt; 5; i++) {
+cout &lt;&lt; marks[i] &lt;&lt; &quot; &quot;;
+}
+return 0;
+}
