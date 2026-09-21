63c36af559795449fb68cf81dcca0ce2830ef3a7
shejaldakhare75-dot <shejaldakhare75@gmail.com>
2026-09-21 22:19:46 +0530

    Added Program 02

---
+++ Unit-01/CodeBook/Program02.cpp
@@ -0,0 +1,12 @@
+#include &lt;iostream&gt;
+using namespace std;
+int main() {
+  int marks = 45;
+  if (marks &gt;= 40) {
+  cout &lt;&lt; &quot;Pass&quot;;
+  }
+  else {
+    cout &lt;&lt; &quot;Fail&quot;;
+  }
+  return 0;
+  }
