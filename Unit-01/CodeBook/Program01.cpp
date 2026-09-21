a34a4f4abf4648bd97784e6cc74c39e6765df8be
shejaldakhare75-dot <shejaldakhare75@gmail.com>
2026-09-21 22:17:40 +0530

    Added Program 01

---
+++ Unit-01/CodeBook/Program01.cpp
@@ -0,0 +1,11 @@
+#include &lt;iostream&gt;
+using namespace std;
+int main() {
+int roll = 101;
+char grade = &#39;A&#39;;
+float fee = 12500.50;
+cout &lt;&lt; &quot;Roll No: &quot; &lt;&lt; roll &lt;&lt; endl;
+cout &lt;&lt; &quot;Grade: &quot; &lt;&lt; grade &lt;&lt; endl;
+cout &lt;&lt; &quot;Fee: &quot; &lt;&lt; fee &lt;&lt; endl;
+return 0;
+}
